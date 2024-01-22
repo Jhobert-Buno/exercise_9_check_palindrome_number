b1e6007c1c74324de48548d0033b04a6dd779795
Jhobert Buno <jhobertbuno02valdez@gmail.com>
2024-01-23 00:58:52 +0800

    given

--- exercise_nine.py
+++ exercise_nine.py
@@ -4,13 +4,22 @@
 # A palindrome number is a number that is the same after reverse. For example, 545, is the palindrome numbers
 
 def palindrome (number):
-    print('the given number is: ', number)
+    print('The given number is: ', number)
     original_number = str(number)
 
     reverse_number = original_number[::-1]
     if original_number == reverse_number:
-        print("Yes. given number is palindrome number")
+        print("Yes. The number is palindrome number")
 
     else:
-        print("No. given number is not palindrome number")
+        print("No. The number is not palindrome number")
+
+# given/example
+print("Example")
+given_1 = 121
+given_2 = 125
+
+palindrome(given_1)
+print('')
+palindrome(given_2)
 
